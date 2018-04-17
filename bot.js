9b6135c0ce70bcd653fb26c16643481fccd3b9b3
JMPuppets <37351112+JMPuppets@users.noreply.github.com>
2018-04-16 20:49:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,32 +16,6 @@ client.on('message', message => {
     	message.channel.send('');
   	}
 });
-.auto .gift={init}
-{embed:
-    {title:Something Something Darkside..}
-    {type:rich}
-    {author|name:{/user}}
-    {author|icon:{usericon}}
-    {author|url:{usericon}}
-    {color:
-        {randlist:
-             #ff0000,#00ff00,#ffffff,#4286f4,
-             #f45642,#262525,#e2d626,#87e226,
-             #26e2c0,#2633e2,#8126e2
-        }
-    }
-    {thumb|url:{guild|icon}}
-    {desc:
-        {randlist2:
-            This should be a response,
-            Along with this!,
-            and now some more,
-            Two Step, One Jump!,
-            Live long!
-        }
-    }
-    {footer|icon:https://xtclabs.net/img/favicon-new.png}
-    {footer|text: Echo 2.0 A.R.S}
-}
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
